394b53a4f08c0bb8d97f9a31267681a368af4e15
saiteja9999 <saitejaboppana9999@gmail.com>
2020-07-31 15:43:00 -0500

    Version 1

---
+++ Website/Assignment1/Default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlinput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlinput.Items[1].Selected ==true)
+            {
+                ddltarget.Items[1].Enabled = false;
+            }
+
+            else if (ddlinput.Items[2].Selected == true)
+            {
+                ddltarget.Items[2].Enabled = false;
+            }
+            else if (ddlinput.Items[3].Selected == true)
+            {
+                ddltarget.Items[3].Enabled = false;
+            }
+            else
+            {
+                ddltarget.Items[4].Enabled = false;
+            }
+        }
+
+        protected void btnconvert_Click(object sender, EventArgs e)
+        {
+            if(IsValid)
+            {
+                if(ddlinput.Items[1].Selected == true && ddltarget.Items[2].Selected==true )
+                {
+                    int num = int.Parse(txtinput.Text);
+                    string Base = Convert.ToString(num, 2);
+                    outputlbl.Text = Base;
+                   
+
+                }
+               else  if(ddlinput.Items[1].Selected ==true && ddltarget.Items[3].Selected==true)
+                {
+                    int num = int.Parse(txtinput.Text);
+                    String Base = Convert.ToString(num, 8);
+                    outputlbl.Text = Base;
+
+                }
+               else if (ddlinput.Items[1].Selected == true && ddltarget.Items[4].Selected == true)
+                {
+                    int num = int.Parse(txtinput.Text);
+                    String Base = Convert.ToString(num, 16);
+                    outputlbl.Text = Base;
+
+                }
+                else if(ddlinput.Items[2].Selected == true && ddltarget.Items[1].Selected==true)
+                {
+                    int num = int.Parse(txtinput.Text);
+                    int decimalValue = Convert.ToInt32(num.ToString(), 2);
+                    outputlbl.Text = decimalValue.ToString();
+
+                }
+                else if(ddlinput.Items[2].Selected==true && ddltarget.Items[3].Selected==true)
+                {
+                    String num = txtinput.Text;
+                    int octal = Convert.ToInt32(num, 2);
+                    outputlbl.Text = Convert.ToString(octal,8);
+
+                }
+                else if(ddlinput.Items[2].Selected == true && ddlinput.Items[4].Selected == true )
+                {
+                    string HexConverted(string strBinary)
+                    {
+                        string strHex = Convert.ToInt32(strBinary, 2).ToString("X");
+                        return strHex;
+                    }
+                    outputlbl.Text = Convert.ToString(HexConverted(txtinput.Text));
+                }
+                else if(ddlinput.Items[3].Selected == true && ddlinput.Items[1].Selected == true)
+                {
+                    int octnum = 0, r = 0;
+
+                    int i = 0;
+
+                    double decnum = 0;
+
+                    octnum = int.Parse(txtinput.Text);
+
+                    while (octnum != 0)
+
+                    {
+
+                        r = octnum % 10;
+
+                        decnum = decnum + (r * Math.Pow(8, i++));
+
+                        octnum = octnum / 10;
+
+                    }
+
+                    outputlbl.Text = decnum.ToString();
+
+                    string newvalue = decnum.ToString();
+                }
+                else if(ddlinput.Items[3].Selected == true && ddlinput.Items[2].Selected == true)
+                {
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Website/Assignment1/obj/Release/Package/PackageTmp/Default.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 40%;
+        }
+        .auto-style2 {
+            width: 292px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblcalculator" runat="server" Font-Bold="True" ForeColor="#FF3300" Text="Calculator"></asp:Label>
+            <br />
+            <br />
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Label ID="lblsource" runat="server" Text="Source" Font-Bold="True" ForeColor="Blue"></asp:Label>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:Label ID="lbltarget" runat="server" Text="Target" Font-Bold="True" ForeColor="Blue"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:DropDownList ID="ddlinput" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlinput_SelectedIndexChanged">
+                            <asp:ListItem>Please Choose One</asp:ListItem>
+                            <asp:ListItem>Decimal</asp:ListItem>
+                            <asp:ListItem>Binary</asp:ListItem>
+                            <asp:ListItem>Octal</asp:ListItem>
+                            <asp:ListItem>Hexadecimal</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:DropDownList ID="ddltarget" runat="server">
+                            <asp:ListItem>Please Choose One</asp:ListItem>
+                            <asp:ListItem>Decimal</asp:ListItem>
+                            <asp:ListItem>Binary</asp:ListItem>
+                            <asp:ListItem>Octal</asp:ListItem>
+                            <asp:ListItem>Hexadecimal</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:TextBox ID="txtinput" runat="server"></asp:TextBox>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:Label ID="outputlbl" runat="server" Text="Output"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Label ID="instatelbl" runat="server" Font-Bold="True" ForeColor="Red" Text="Please Enter Your Input here"></asp:Label>
+                    </td>
+                    <td class="auto-style2">
+                        <asp:Label ID="outstatlbl" runat="server" Font-Bold="True" ForeColor="Red" Text="Your Output Appears Here"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                        <asp:Button ID="btnconvert" runat="server" Text="Convert" OnClick="btnconvert_Click" />
+                    </td>
+                    <td class="auto-style2">
+                        <asp:Button ID="btnclear" runat="server" Text="Clear" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">&nbsp;</td>
+                    <td class="auto-style2">&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
